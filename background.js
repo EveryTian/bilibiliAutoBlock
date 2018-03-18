4d3323c0bc092a158a1f0b896d2c54caec7ff4bb
RenHaotian <haotian_ren@outlook.com>
2018-03-18 20:23:02 +0800

    Add interface for block record

--- background.js
+++ background.js
@@ -47,7 +47,10 @@ function analyze(xmlString) {
     debugLog('Blocked Patterns:', blockedPatterns);
     let blockedDanmaku = [].filter.call(danmakuElements, x => danmakuBlockCheck(x.innerHTML));
     debugLog('Blocked Danmaku:', blockedDanmaku);
-    let blockedUsers = [].map.call(blockedDanmaku, x => x.getAttribute('p').split(',')[6]);
+    let blockedUsers = {};
+    for (let i = 0; i < blockedDanmaku.length; ++i) {
+        blockedUsers[blockedDanmaku[i].getAttribute('p').split(',')[6]] = blockedDanmaku[i].innerHTML;
+    }
     debugLog('Blocked Users:', blockedUsers);
     submitBlockedUsers(blockedUsers);
 }
@@ -69,20 +72,33 @@ function danmakuBlockCheck(danmakuContent) {
 }
 
 function submitBlockedUsers(blockedUsers) {
-    function submitBlockedUser(blockedUser) {
+    debugLog('Submitting Blocked Users...');
+    let blockedUsersIdArray = [];
+    for (let blockedUser in blockedUsers) {
+        if (blockedUsers.hasOwnProperty(blockedUser)) {
+            blockedUsersIdArray.push(blockedUser);
+        }
+    }
+    blockedUsersIdArray.forEach(blockedUser => {
         let xmlHttpRequest = new XMLHttpRequest();
         debugLog(' | Submitting `' + blockedUser + '`...');
         xmlHttpRequest.withCredentials = true;
         xmlHttpRequest.onreadystatechange = () => {
-            // TODO
+            debugLog('     | `' + blockedUser + '` readyState:', xmlHttpRequest.readyState);
+            debugLog('     | `' + blockedUser + '` status:', xmlHttpRequest.status);
+            if (xmlHttpRequest.readyState === 4 && xmlHttpRequest.status === 200) {
+                // {"code":0,"data":{"id":\d{6},"mid":0,"type":0,"filter":"","comment":""},"message":"0","ttl":1}
+                let blockResponse = JSON.parse(xmlHttpRequest.responseText);
+                if (blockResponse.code === 0) {
+                    debugLog('     | Block `' + blockedUser + '(' + blockedUsers[blockedUser] + ')` Successfully.');
+                    // TODO
+                }
+            }
         };
         xmlHttpRequest.open('POST', 'https://api.bilibili.com/x/dm/filter/user/add', true);
         xmlHttpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
         xmlHttpRequest.send('type=2&filter=' + blockedUser + '&jsonp=jsonp&csrf');
-    }
-
-    debugLog('Submitting Blocked Users...');
-    blockedUsers.forEach(submitBlockedUser);
+    });
 }
 
 function isString(obj) {
